18cfce7208883686df9fa0bbf135528a58085a22
LiveForCode <61900769+Abhay-Shankur@users.noreply.github.com>
2023-04-01 15:41:27 +0530

    First Commit

---
+++ app/src/main/java/com/application/zomato/SignuppageActivity.java
@@ -0,0 +1,14 @@
+package com.application.zomato;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignuppageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signuppage);
+    }
+}
